c9a8d169d63c61bdf7acbd6a6730c4504b0a544f
Floride <florian.reimat@orange.fr>
2020-08-30 21:58:08 +0200

    First Commit

---
+++ install_zsh.sh
@@ -0,0 +1,9 @@
+# Install zsh
+sudo apt-get install zsh
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Choose Theme
+
+# Install Theme
+
+# power git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k

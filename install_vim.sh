dd9c226b81ca47b32c94a710300595975a38f789
Floride <florian.reimat@orange.fr>
2020-08-31 16:55:02 +0200

    Updating dotfile + AutoInstall V0.01

---
+++ install_vim.sh
@@ -0,0 +1,11 @@
+# Install Vim
+sudo apt install vim
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Set Up .vimrc file
+sudo cp ~/.vimrc ./.old_vimrc
+sudo mv ./.vimrc ~/.vimrc
+
+
--- install_zsh.sh
+++ install_zsh.sh
@@ -6,4 +6,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # Install Theme
 
-# power git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+# power 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git 
+${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k

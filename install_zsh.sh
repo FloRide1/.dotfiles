5f08e35fb1d051fc7caebdfd85bbc3dec59f7e42
Floride <florian.reimat@orange.fr>
2020-09-10 23:00:21 +0200

    Fix zsh installation

--- install_zsh.sh
+++ install_zsh.sh
@@ -8,11 +8,13 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Install Theme
 
 # Power10k 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-
-mv .zshrc ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+source .zshrc
 
 p10k configure
+
+source .zshrc
+cp .zshrc ~/.zshrc
 
 # Set Font
 mv ./Fonts/*.ttf ~/.local/share/fonts

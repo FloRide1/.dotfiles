becd8a60ba94c7925ade6b04b0a5c77bd7625934
FloRide1 <florian.reimat@orange.fr>
2021-03-19 22:03:27 +0100

    Debug the install script

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+copy_and_link () {
+	CURRENT_PATH=$(dirname $(realpath $0))
+	mkdir "${CURRENT_PATH}/Old_File"
+	cp -r "${HOME}/$1" "${CURRENT_PATH}/Old_File/$1"
+	ln -s "${HOME}/$1" "${CURRENT_PATH}/$1"
+}
 # Install .vimrc file
 copy_and_link .vimrc
 
 # Install .zshrc file
 copy_and_link .zshrc
+copy_and_link .p10k.zsh
 
 # Install .gitconfig file
 copy_and_link .gitconfig
 
-
-copy_and_link () {
-	mkdir "${PWD}/Old_File"
-	cp -r "${HOME}/$1" "${PWD}/Old_File/$1"
-	ln -s "${HOME}/$1" "${PWD}/$1"

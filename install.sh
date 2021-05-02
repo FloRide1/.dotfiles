aeb349449f28c0b5035e4bb78020ff8d5d7fef42
FloRide1 <florian.reimat@orange.fr>
2021-05-02 14:12:50 +0200

    Add a TODO list inside my install.sh script

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ copy_and_link () {
 	cp -r "${HOME}/$1" "${CURRENT_PATH}/Old_File/$1"
 	ln -sf "${CURRENT_PATH}/${2-.}/$1" "${HOME}/$1"
 }
+
+# TODO: Add vim / neovim installation
+# TODO: Add zsh installation
+# TODO: Add oh-my-zsh installation
+
 # Install .vimrc file
 copy_and_link .vimrc vim
 
